533404040c19f2bc00a6da2e65511b5dc6d0d8f5
ssakethraju <ssakethraj@gmail.com>
2023-09-12 23:46:01 -0500

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.description}>SOAL's ONEST Landing page</h1>
+      <h1 className={styles.description}>SOAL ONEST Landing page</h1>
     </main>
   );
 }
